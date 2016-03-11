f333a84304d8412d8e5b449cc889dc513e3b87f6
Stanislav Stipl <stanislav.stipl@gmail.com>
2016-03-11 22:28:56 +0100

    Martin Kučera

--- reset.sh
+++ reset.sh
@@ -3,4 +3,4 @@ dropdb helios
 createdb helios
 python manage.py syncdb
 python manage.py migrate
-echo "from helios_auth.models import User; User.objects.create(user_type='pirateid',user_id='stanislav.stipl@pirati.cz', info={'name':'Stanislav Stipl'})" | python manage.py shell
+echo "from helios_auth.models import User; User.objects.create(user_type='pirateid',user_id='martin.kucera@pirati.cz', info={'name':'Martin Kučera'})" | python manage.py shell
